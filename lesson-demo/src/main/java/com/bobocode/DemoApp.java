6ae28297ac32f39ebf26a7c3c7aa774c67914346
vlast <lastovskyi.volodymyr@gmail.com>
2022-06-01 18:03:36 +0300

    crazy generics and lambdas

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -1,15 +1,7 @@
 package com.bobocode;
 
-import com.bobocode.data.Accounts;
-
-import java.time.LocalDate;
-
 public class DemoApp {
-    public static void main(String[] args) {
-        var accounts = Accounts.generateAccountList(10);
-        var emailToBirthdayTable = new HashTable<String, LocalDate>();
-        accounts.forEach(a -> emailToBirthdayTable.put(a.getEmail(), a.getBirthday()));
-        emailToBirthdayTable.printTable();
 
+    public static void main(String[] args) {
     }
 }
